ed3178f2f759b2085ba8ba8111f3f7f2565097ae
lipengyuan <lipengyuan1993@gmail.com>
2018-12-10 17:17:53 +0800

    first commit

---
+++ src/test/java/com/lpy/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lpy.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
